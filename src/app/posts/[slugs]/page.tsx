1117f153cfd87c1a4f6d1c6b9449ad698da200a9
Rafael Velazco <rjvelazco21@gmail.com>
2024-06-12 22:18:04 -0400

    feat: render post dynamically

--- src/app/posts/[slugs]/page.tsx
+++ src/app/posts/[slugs]/page.tsx
@@ -1,7 +1,9 @@
-// const path = require('path');
 import path from "path";
 import { readFile } from "node:fs";
 import { promisify } from "util";
+
+import { notFound } from "next/navigation";
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 const postsPath = path.join(process.cwd(), "/src/posts");
 const readFileAsync = promisify(readFile);
@@ -15,9 +17,10 @@ const getPost = async (post: string) => {
 export default async function Home() {
   try {
     const mdxText = await getPost("test");
-  } catch (e) {
-    console.log(e);
-  }
 
-  return <h1>Post</h1>;
+    return <MDXRemote source={mdxText} />;
+
+  } catch (e) {
+    notFound();
+  }
 }
